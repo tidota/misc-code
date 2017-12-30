c7c57b78505567f0265e9df8317bc6eff79934cb
Tetsuya Idota <tidota@hawaii.edu>
2017-12-29 17:37:10 -1000

    completed bash script to list up files under the given directory

--- really_miscellaneous/scripts/for_all_files.sh
+++ really_miscellaneous/scripts/for_all_files.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-echo "$0 and $1"
+###############################################################
+# list up files under the directory specified by the argument #
+###############################################################
+
+# remove a white space from a set of delimiters
+# only a line feed and a tab character are used as delimiters
+IFS_BUFF=$IFS
+IFS=$'\n'$'\t'
+
+# for loop to display each entry from the given command
+for f in `ls -1 $1`
+do
+    echo "file: '$f'"
+done
+
+# recovery of the delimiter set
+IFS=$IFS_BUFF
 
