10afe9f44ddb7672f4065f9f4734afe0aba429b3
Tetsuya Idota <tidota@hawaii.edu>
2018-01-01 02:58:54 -1000

    basic commands added

--- wastetris/format_macro.hpp
+++ wastetris/format_macro.hpp
@@ -1,3 +1,38 @@
 #ifndef _FORMAT_MACRO_HPP
 #define _FORMAT_MACRO_HPP
+
+// ====================================================================== //
+// definitions for escape sequences to provide formats
+// ====================================================================== //
+
+// starting character for escape sequences
+#define ESC '\x001B'
+
+// === positions === //
+// the initial position
+#define START_LOC "[0;0H"
+
+// === colors === //
+// color: black
+#define BLACK "[0m"
+// color: green
+#define GREEN "[32m"
+
+// === misc === //
+// indentation
+#define IND "   "
+// clear the screen
+#define CLEAR "[2J"
+
+// === macro func === //
+#define CLEAR_SCREEN() \
+do{ cout << ESC << CLEAR; }while(0)
+
+#define CHANGE_COLOR_BLACK() \
+do{ cout << BLACK; }while(0)
+
+#define CHANGE_COLOR_GREEN() \
+do{ cout << GREEN; }while(0)
+
+
 #endif //_FORMAT_MACRO_HPP
