b30a7e72f2a7d2c1e8177221bec61256db389f8f
Tetsuya Idota <tidota@hawaii.edu>
2018-01-01 01:54:17 -1000

    comments to the main

--- wastetris/wastetris.cpp
+++ wastetris/wastetris.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+// ============================================================================== //
+// main
+//
+// Description:
+//   The main function first sets up the environment to a non-canonical mode.
+//   It then repeatedly interacts with the user while updating the state and display.
+//   When the user tells to stop or the interaction is supposed to be end,
+//   it breaks the loop.
+//   At the end, it does the final task including deletion of the executable itself.
+// 
+// The condition to break the loop:
+//   when the interaction is supposed to end, e.g., the game is over.
+//   when the user gives Ctrl-D.
+//
+// ============================================================================== //
 int main()
 {
     int f_fail = set_input_mode();
@@ -13,9 +28,13 @@ int main()
     {
         char c = readOneChar();
         if(c != 0x04)
+        {
             cout << c << flush;
+        {
         else
+        {
             f_continue = 0;
+        }
     }
 
     return f_fail;
