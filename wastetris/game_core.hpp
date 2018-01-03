4d4595c391e8a1a5b38a2fb9acd4aa4987f44fed
Tetsuya Idota <tidota@hawaii.edu>
2018-01-02 20:55:24 -1000

    annotated member variables in game_core.hpp

--- wastetris/game_core.hpp
+++ wastetris/game_core.hpp
@@ -7,30 +7,40 @@
 class GAME
 {
 private:
-    GAME();
-    ~GAME();
-
+    // the size of the entire screen
     int screen_width;
     int screen_height;
 
+    // the top-left corner of the box where cells appear
     int bin_start_x;
     int bin_start_y;
 
+    // # of rows and # of columns of the box in terms of cells 
     int nrow;
     int ncol;
 
+    // the top-left corner of the next box
     int next_start_x;
     int next_start_y;
 
-    void draw_background();
-
+    // the status of the game
+    // 0: stopped
+    // 1: running
+    // others: some error or anything else
     int f_stat;
 
-    void update();
+    // thread and mutex
     std::thread t_update;
     std::mutex mtx;
 
+    // pointer to the object (since this class is supposed to be singleton)
     static GAME* game;
+
+    GAME();
+    ~GAME();
+    void draw_background();
+    void update();
+
 public:
     static GAME* init_game();
     static void kill_game();
