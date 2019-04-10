e29a1251244f52c04b1f87036b8eebfe989bb885
Tetsuya Idota <tidota@hawaii.edu>
2019-04-10 04:08:01 -1000

    xlnt: read and write

--- snippets/c_cpp/xls/test_xlnt.cpp
+++ snippets/c_cpp/xls/test_xlnt.cpp
@@ -1,16 +1,32 @@
+#include <iostream>
+
 #include <xlnt/xlnt.hpp>
 
 int main()
 {
+  std::string filename = "./example.xlsx";
+  try
+  {
+    std::cout << "Opening: " << filename << std::endl;
     xlnt::workbook wb;
+    wb.load(filename);
     xlnt::worksheet ws = wb.active_sheet();
-    ws.cell("A1").value(5);
-    ws.cell("B2").value("string data");
-    ws.cell("C3").formula("=RAND()");
-    ws.cell(10,10).value("insersion by indexes?");
-    ws.merge_cells("C3:C4");
-    ws.freeze_panes("B2");
-    wb.save("example.xlsx");
-    return 0;
+    std::cout << "A1: " << ws.cell(1,1).value<int>() << std::endl;
+  }
+  catch(...)
+  {
+    std::cout << "File not found: " << filename << std::endl;
+  }
+  std::cout << "Writing: " << filename << std::endl;
+  xlnt::workbook wb;
+  xlnt::worksheet ws = wb.active_sheet();
+  ws.cell("A1").value(5);
+  ws.cell("B2").value("string data");
+  ws.cell("C3").formula("=RAND()");
+  ws.cell(10,10).value("insersion by indexes?");
+  ws.merge_cells("C3:C4");
+  ws.freeze_panes("B2");
+  wb.save(filename);
+  return 0;
 }
 // compile with -std=c++14 -Ixlnt/include -lxlnt
