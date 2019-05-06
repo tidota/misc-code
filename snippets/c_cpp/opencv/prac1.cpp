cd2628ccd694300de67b514ba8ac1c61c4e69e61
Tetsuya Idota <tidota@hawaii.edu>
2019-05-05 19:52:21 -1000

    opencv: prac1 (dark image)

--- snippets/c_cpp/opencv/prac1.cpp
+++ snippets/c_cpp/opencv/prac1.cpp
@@ -15,13 +15,37 @@ int main(int argc, char** argv )
         printf("No image data \n");
         return -1;
     }
+
     Mat gray_image;
     cvtColor(image, gray_image, COLOR_RGB2GRAY);
 
+    Mat dark_image = image.clone();
+    int channels = dark_image.channels();
+    int rows = dark_image.rows;
+    int cols = dark_image.cols * channels;
+
+    if (dark_image.isContinuous())
+    {
+      cols *= rows;
+      rows = 1;
+    }
+    int i, j;
+    uchar* p;
+    for (i = 0; i < rows; ++i)
+    {
+      p = dark_image.ptr<uchar>(i);
+      for (j = 0; j < cols; ++j)
+      {
+        p[j] = p[j]/2;
+      }
+    }
+
     namedWindow("Original", WINDOW_AUTOSIZE );
     namedWindow("Gray scale", WINDOW_AUTOSIZE );
+    namedWindow("Dark", WINDOW_AUTOSIZE );
     imshow("Original", image);
     imshow("Gray scale", gray_image);
+    imshow("Dark", dark_image);
     waitKey(0);
     return 0;
 }
