c703fbbd96fc6880a216b35fbec8f5d562ba126d
Tetsuya Idota <tidota@hawaii.edu>
2019-05-05 22:24:53 -1000

    opencv: prac1 (blending and modding)

--- snippets/c_cpp/opencv/prac1.cpp
+++ snippets/c_cpp/opencv/prac1.cpp
@@ -19,6 +19,7 @@ int main(int argc, char** argv )
     std::cout << "Gray scaling..." << std::endl;
     Mat gray_image;
     cvtColor(image, gray_image, COLOR_RGB2GRAY);
+    // image.converTo(gray_image, CV_32F);
 
     std::cout << "Making it darker..." << std::endl;
     Mat dark_image = image.clone();
@@ -46,14 +47,30 @@ int main(int argc, char** argv )
     Mat kernel = (Mat_<char>(3,3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
     filter2D(image, sharp_image, image.depth(), kernel);
 
+    std::cout << "Arbitrarily modding..." << std::endl;
+    Mat mod_image = image.clone();
+    Mat mod_image_select = mod_image(Rect(5, 5, 20, 20));
+    mod_image_select = Vec3b(0,0,0);
+    line(mod_image, Point2f(0, 0), Point2f(30, 15), Scalar(0, 1, 0), 2, LINE_8);
+
+    std::cout << "Blending two images..." << std::endl;
+    Mat blended_image;
+    Mat gray_image_temp;
+    cvtColor(gray_image, gray_image_temp, COLOR_GRAY2RGB);
+    addWeighted(image, 0.3, gray_image_temp, 0.7, 0.0, blended_image);
+
     namedWindow("Original", WINDOW_AUTOSIZE );
-    namedWindow("Gray scale", WINDOW_AUTOSIZE );
-    namedWindow("Dark", WINDOW_AUTOSIZE );
-    namedWindow("Sharp", WINDOW_AUTOSIZE );
     imshow("Original", image);
+    namedWindow("Gray scale", WINDOW_AUTOSIZE );
     imshow("Gray scale", gray_image);
+    namedWindow("Dark", WINDOW_AUTOSIZE );
     imshow("Dark", dark_image);
+    namedWindow("Sharp", WINDOW_AUTOSIZE );
     imshow("Sharp", sharp_image);
+    namedWindow("Mod", WINDOW_AUTOSIZE );
+    imshow("Mod", mod_image);
+    namedWindow("Blended", WINDOW_AUTOSIZE );
+    imshow("Blended", blended_image);
     waitKey(0);
     return 0;
 }
