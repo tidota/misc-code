9b43a3b6c2acadbcc6ccfb5f968a8a42a9a0bea5
Tetsuya Idota <tidota@hawaii.edu>
2019-12-05 23:41:26 -1000

    openmp: bugfix

--- snippets/c_cpp/multithread/openmp/openmp.cpp
+++ snippets/c_cpp/multithread/openmp/openmp.cpp
@@ -3,7 +3,6 @@
 // this code is just for practice.
 // 
 #include <iostream>
-#include <mutex>
 #include <omp.h>
 
 using namespace std;
@@ -12,12 +11,21 @@ int main()
 {
     cout << "start" << endl;
 
-    mutex mtx;
-
     #pragma omp parallel num_threads(10)
-    mtx.lock();
-    cout << "id: " << omp_get_thread_num() << endl;
-    mtx.unlock();
+    {
+        #pragma omp critical
+        {
+            cout << "1st half id: " << omp_get_thread_num() << endl;
+        }
+
+        for (int i = 0; i < 100000; ++i){}
+
+        #pragma omp critical
+        {
+            cout << "2nd half id: " << omp_get_thread_num() << endl;
+        }
+    }
+    cout << "end" << endl;
 
     return 0;
 }
