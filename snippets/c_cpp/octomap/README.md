51ff2f3f9f074cab2da34f56a30db25c376f4245
Tetsuya Idota <tidota@hawaii.edu>
2020-03-05 17:32:33 -1000

    octomap: instantiate map

--- snippets/c_cpp/octomap/README.md
+++ snippets/c_cpp/octomap/README.md
@@ -1,7 +1,13 @@
-# Octomap
+# Octomap practice
 
 ## installation
 
 https://github.com/OctoMap/octomap/wiki/Compilation-and-Installation-of-OctoMap
 
+`liboctomap-dev` may be necessary to be installed.
+
+## TODO
+
+- [ ] add random data
+- [ ] visualize the data
 
--- snippets/c_cpp/octomap/prac.cpp
+++ snippets/c_cpp/octomap/prac.cpp
@@ -6,5 +6,15 @@
 int main(int argc, char** argv )
 {
 
-  return 0;
+    octomap::OcTree *map;
+
+    map = new octomap::OcTree(0.25);
+    map->setProbHit(0.7);
+    map->setProbMiss(0.4);
+    map->setClampingThresMin(0.12);
+    map->setClampingThresMax(0.97);
+
+    delete map;
+
+    return 0;
 }
