605393933de759da5dc2795cbb35b34a879850d2
Tetsuya Idota <tidota@hawaii.edu>
2020-06-17 01:59:41 -1000

    eigen: block

--- snippets/c_cpp/eigen/prac.cpp
+++ snippets/c_cpp/eigen/prac.cpp
@@ -12,5 +12,17 @@ int main()
   A(1,1) = 5;
   std::cout << "A(1,1) = 5: " << std::endl << A << std::endl;
 
+
+  MatrixXd B = MatrixXd::Zero(4, 4);
+  Ref<MatrixXd> subB = B.block(1,1,3,3);
+  std::cout << "B: " << std::endl << B << std::endl;
+  std::cout << "subB: " << std::endl << subB << std::endl;
+  std::cout << "mod on B: B(1,1) = 5" << std::endl;
+  B(1,1) = 5;
+  std::cout << "mod on subB: subB(2,2) = 9" << std::endl;
+  subB(2,2) = 9;
+  std::cout << "B: " << std::endl << B << std::endl;
+  std::cout << "subB: " << std::endl << subB << std::endl;
+
   return 0;
 }
