411b189eee5501dd5bcc42ca6da2ad5567da1e4e
Tetsuya Idota <tidota@hawaii.edu>
2020-08-01 15:53:22 -1000

    eigen: prac on vector x scalar

--- snippets/c_cpp/eigen/prac.cpp
+++ snippets/c_cpp/eigen/prac.cpp
@@ -24,5 +24,14 @@ int main()
   std::cout << "B: " << std::endl << B << std::endl;
   std::cout << "subB: " << std::endl << subB << std::endl;
 
+  VectorXd v = VectorXd::Zero(3);
+  v(0) = 3;
+  v(1) = 5;
+  v(2) = 7;
+  std::cout << "v: " << std::endl << v << std::endl;
+  std::cout << "v * 5" << std::endl;
+  v = v * 5;
+  std::cout << "v: " << std::endl << v << std::endl;
+
   return 0;
 }
