e707c7b4b586cd5a9f2093096b033a2dbe3917b7
Tetsuya Idota <tidota@hawaii.edu>
2021-07-07 22:45:56 -1000

    cpp11: try capture of lambda

--- c_cpp/cpp11_more/lambda.cpp
+++ c_cpp/cpp11_more/lambda.cpp
@@ -24,24 +24,53 @@ public:
 
 int main()
 {
-    auto f = [](){ cout << "success!" << endl; };
+    {
+        cout << "========================================================" << endl;
+        cout << "calling a lambda function accessing an outside item" << endl;
+        std::string str = "outside message";
+        auto f = [&](){ cout << str << endl; cout << "success!" << endl; };
+        f();
+    }
 
-    f();
+    {
+        cout << "========================================================" << endl;
+        cout << "calling a lambda function taking two rvalues" << endl;
+        auto f = [](auto&& a, auto&& b){ return a + b; };
+        cout << "f2(3,7)= " << f(3,7) << endl;
+        int a = 5;
+        cout << "f2(a,7)= " << f(a,7) << endl;
+    }
 
-    auto f2 = [](auto&& a, auto&& b){ return a + b; };
+    {
+        cout << "========================================================" << endl;
+        cout << "calling a lambda function taking two lvalues" << endl;
+        auto f = [](auto& a, auto& b){ return a.num + b.num; };
+        NUM c(7);
+        NUM d(8);
+        int result = f(c,d);
+        cout << "f3(NUM(7), NUM(8))= " << result << endl;
+    }
 
-    cout << "f2(3,7)= " << f2(3,7) << endl;
-
-    int a = 5;
-
-    cout << "f2(a,7)= " << f2(a,7) << endl;
-
-    auto f3 = [](auto& a, auto& b){ return a.num + b.num; };
-    NUM c(7);
-    NUM d(8);
-    int result = f3(c,d);
-    cout << "f3(NUM(7), NUM(8))= " << result << endl;
-
+    {
+        cout << "========================================================" << endl;
+        NUM c(7);
+        cout << "c.num = " << c.num << endl;
+        cout << "calling a lambda function modifying a copied item" << endl;
+        auto f = [=]() mutable { c.num = 10; };
+        f();
+        cout << "c.num = " << c.num << endl;
+    }
+    
+    {
+        cout << "========================================================" << endl;
+        NUM c(7);
+        cout << "c.num = " << c.num << endl;
+        cout << "calling a lambda function modifying a reference" << endl;
+        auto f = [&](){ c.num = 10; };
+        f();
+        cout << "c.num = " << c.num << endl;
+    }
+    
     return 0;
 }
 
