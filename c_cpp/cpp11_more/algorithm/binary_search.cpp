3955c7f07e2a7d77f0ea53df6413bee7418293ae
Tetsuya Idota <tidota@hawaii.edu>
2021-09-17 11:54:22 -1000

    algorithm: update binary_search

--- c_cpp/cpp11_more/algorithm/binary_search.cpp
+++ c_cpp/cpp11_more/algorithm/binary_search.cpp
@@ -15,31 +15,69 @@ ostream& operator<<(ostream& out, T<N> container)
 
 int main()
 {
-  vector<int> vec1 = {2, 3, 10, 5, 6, 5, 5};
-  set<int> s;
-  for (auto item: vec1) s.insert(item);
-
-  cout << "vec1: " << vec1 << endl;
-  cout << "set: " << s << endl;
-
+  cout << "================================================" << endl;
   {
-    cout << "set (4 <= item < 6): ";
-    auto it_start = lower_bound(s.begin(), s.end(), 4);
-    auto it_end = upper_bound(s.begin(), s.end(), 6);
-    for (auto it = it_start; it != it_end; ++it)
-      cout << *it << ", ";
+    vector<int> vec = {2, 3, 10, 5, 6, 5, 5, 4, 4, 6, 6};
+    cout << " vector: " << endl << vec << endl;
+    std::sort(vec.begin(), vec.end());
+    cout << " vector (sorted): " << endl << vec << endl;
+    {
+      cout << "vec (4 <= item <= 6): ";
+      auto it_start = lower_bound(vec.begin(), vec.end(), 4);
+      auto it_end = upper_bound(vec.begin(), vec.end(), 6);
+      for (auto it = it_start; it != it_end; ++it)
+        cout << *it << ", ";
+      cout << endl;
+      cout << "beginnin: " << (it_start - vec.begin()) << endl;
+      cout << "last:     " << (it_end - vec.begin()) << endl;
+    }
     cout << endl;
-  }
-  {
-    cout << "set (item == 5): ";
-    auto range = equal_range(s.begin(), s.end(), 5);
-    for (auto it = range.first; it != range.second; ++it)
-      cout << *it << ", ";
-    cout << endl;
+    auto item = binary_search(vec.begin(), vec.end(), 4, [](int a, int b){ return a + 1 == b; });
+    cout << "found for 4?: " << item << endl << endl;
   }
 
-  auto item = binary_search(vec1.begin(), vec1.end(), 4, [](int a, int b){ return a + 1 == b; });
-  cout << "found for 4?: " << item << endl;
+  {
+    vector<int> vec = {2, 3, 10, 5, 5, 5, 1, 9, 1};
+    cout << " vector: " << endl << vec << endl;
+    std::sort(vec.begin(), vec.end());
+    cout << " vector (sorted): " << endl << vec << endl;
+    {
+      cout << "vec (4 <= item <= 6): ";
+      auto it_start = lower_bound(vec.begin(), vec.end(), 4);
+      auto it_end = upper_bound(vec.begin(), vec.end(), 6);
+      for (auto it = it_start; it != it_end; ++it)
+        cout << *it << ", ";
+      cout << endl;
+      cout << "beginnin: " << (it_start - vec.begin()) << endl;
+      cout << "last:     " << (it_end - vec.begin()) << endl;
+    }
+    cout << endl;
+    auto item = binary_search(vec.begin(), vec.end(), 4, [](int a, int b){ return a + 1 == b; });
+    cout << "found for 4?: " << item << endl << endl;
+  }
 
+  cout << "================================================" << endl;
+  {
+    set<int> s;
+    for (auto item: {2, 3, 10, 5, 5, 5, 1, 9, 1}) s.insert(item);
 
+    cout << "set: " << s << endl << endl;
+    {
+      cout << "set (4 <= item <= 6): ";
+      auto it_start = lower_bound(s.begin(), s.end(), 4);
+      auto it_end = upper_bound(s.begin(), s.end(), 6);
+      for (auto it = it_start; it != it_end; ++it)
+        cout << *it << ", ";
+      cout << endl;
+    }
+    {
+      cout << "set (item == 5): ";
+      auto range = equal_range(s.begin(), s.end(), 5);
+      for (auto it = range.first; it != range.second; ++it)
+        cout << *it << ", ";
+      cout << endl;
+    }
+  }
+
+  return 0;
 }
