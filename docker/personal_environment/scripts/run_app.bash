69966bf7e1fa92b8932095b062e3190450a6a061
Tetsuya Idota <tidota@hawaii.edu>
2022-09-25 21:59:59 -0700

    update the docker scripts

--- docker/personal_environment/scripts/run_app.bash
+++ docker/personal_environment/scripts/run_app.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 container=main_container
+command=`echo $1 | awk '{split($0,a," "); print a[1]}'`
+command=`echo $command | sed "s/\//_/g"`
 if [[ $# -eq 0 ]]; then
 docker exec -it $container bash
 else
-docker exec $container $1
+docker exec $container $1 > /mnt/c/Users/Tetsuya/Documents/management/docker_"$command"_log.txt
 fi
 
