1a377f50a7a8a5a19d50dcaedd35ef2c12a86f8a
Tetsuya Idota <tidota@hawaii.edu>
2022-10-02 00:15:16 -0700

    update the docker scripts

--- docker/personal_environment/scripts/start_container.bash
+++ docker/personal_environment/scripts/start_container.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 hostip=$(ipconfig.exe | grep 'vEthernet (WSL)' -A4 | cut -d":" -f 2 | tail -n1 | sed -e 's/\s*//g')
-win_home="/mnt/c/Users/Tetsuya:/media/win_home"
+#win_home="/mnt/c/Users/Tetsuya:/media/win_home"
+win_home="/mnt/c/Users/Tetsuya:/home/developer/win_home"
+gitconfig="/home/tidota/.gitconfig:/home/developer/.gitconfig"
 ssh_dir="/home/tidota/.ssh:/home/developer/.ssh"
 time_zone=America/Los_Angeles
 #image="tidota/ubuntu-personal"
@@ -10,6 +12,7 @@ docker run \
   -it --rm \
   --name $container \
   -v $win_home \
+  -v $gitconfig \
   -v $ssh_dir \
   -e TZ=$time_zone \
   -e DISPLAY=$hostip:0.0 \
