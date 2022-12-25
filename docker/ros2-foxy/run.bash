808ddfc9f21c6f740754a804fd868101517436a2
Tetsuya Idota <tidota@hawaii.edu>
2022-12-25 02:20:19 -0800

    remove xauth lines from run.bash

--- docker/ros2-foxy/run.bash
+++ docker/ros2-foxy/run.bash
@@ -38,21 +38,6 @@ echo "Image: $1"
 ARGS=("$@")
 WORKSPACES=("${ARGS[@]:1}")
 
-# Make sure processes in the container can connect to the x server
-# Necessary so gazebo can create a context for OpenGL rendering (even headless)
-XAUTH=/tmp/.docker.xauth
-if [ ! -f $XAUTH ]
-then
-    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
-    if [ ! -z "$xauth_list" ]
-    then
-        echo $xauth_list | xauth -f $XAUTH nmerge -
-    else
-        touch $XAUTH
-    fi
-    chmod a+r $XAUTH
-fi
-
 DOCKER_OPTS=
 
 # Share your vim settings.
