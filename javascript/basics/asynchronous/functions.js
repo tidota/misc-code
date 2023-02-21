57a62ab9bf1417b0d46bcd1aca22fa1e1b4e6171
Tetsuya Idota <tidota@hawaii.edu>
2023-02-20 22:59:39 -0800

    js: add asynchronous prac

---
+++ javascript/basics/asynchronous/functions.js
@@ -0,0 +1,9 @@
+function updateClock() {
+  let d = new Date();
+  document.getElementById("demo").innerHTML=
+  d.getHours() + ":" +
+  d.getMinutes() + ":" +
+  d.getSeconds();
+}
+
+setInterval(updateClock, 1000);
