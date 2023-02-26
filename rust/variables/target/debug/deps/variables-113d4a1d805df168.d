75735276175932f5cca0c800663b4e21d9361f5a
Tetsuya Idota <tidota@hawaii.edu>
2023-02-26 01:54:17 -0800

    rust: add variables project

---
+++ rust/variables/target/debug/deps/variables-113d4a1d805df168.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Tetsuya/git/misc-code/rust/variables/target/debug/deps/variables-113d4a1d805df168: src/main.rs
+
+/mnt/c/Users/Tetsuya/git/misc-code/rust/variables/target/debug/deps/variables-113d4a1d805df168.d: src/main.rs
+
+src/main.rs:
---
+++ rust/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Tetsuya/git/misc-code/rust/variables/target/debug/variables: /mnt/c/Users/Tetsuya/git/misc-code/rust/variables/src/main.rs
